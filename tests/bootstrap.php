5b9d16b35adc0fd0677407b8390b386939c1da4c
Harry Walter <harry@ukwebmedia.com>
2012-07-24 11:00:42 +0100

    Correct autoload path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function($class)
 {
     if (0 === strpos($class, 'Versionable\\')) {
-        $file = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
+        $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
         if (file_exists($file)) {
             require_once $file;
             return true;
